1474465d9e88e5397a09a9fa1b33299856eacdf1
SHxBHAM <shubhammishra41880@gmail.com>
2024-01-30 10:14:58 +0530

    lol

---
+++ settimeout-callback.js
@@ -0,0 +1,9 @@
+function parent(name, callback){
+    setTimeout(callback, 1000);
+    console.log("nice pic deer "+name);
+}
+
+
+parent("nigga",()=> {
+        console.log("mera fir se bulawa aagya");
+});
