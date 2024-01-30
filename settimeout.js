47b6c11f441b040d99dbc7860420182f149551ae
SHxBHAM <shubhammishra41880@gmail.com>
2024-01-30 09:33:48 +0530

    class notes

--- settimeout.js
+++ settimeout.js
@@ -3,3 +3,13 @@ setTimeout(() => {
     console.log("this printed after 2s");
 }, 2000);
 console.log("THIS IS AFTER THE SETTIMEOUT");// printed first because settimeout is an async function out of the box.
+
+// -----------------------------------SET INTERVAL & CLEAR INTERVAL---------------------------------------------------------------------------
+function repeater(){
+    console.log("executing repeater");
+}
+const lmao = setInterval(repeater, 2000);
+setTimeout(() => {
+    clearInterval(lmao);
+    console.log("stopped");
+}, 20000);
